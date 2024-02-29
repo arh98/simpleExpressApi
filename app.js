0d855b2b2d73246787bc9ed1fd7a238978791a4d
Arh <a.hafizi9@outlook.com>
2024-02-29 17:14:37 +0330

    init

---
+++ app.js
@@ -0,0 +1,62 @@
+const path = require("path"),
+  express = require("express"),
+  mongoose = require("mongoose"),
+  multer = require("multer"),
+  feedRoutes = require("./routes/feed");
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(express.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/api/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .set("strictQuery", false)
+  .connect("mongodb://localhost:27017/feedsdb")
+  .then((result) => {
+    app.listen(3000);
+    console.log('Connected to DB.');
+  })
+  .catch((err) => console.log(err));
