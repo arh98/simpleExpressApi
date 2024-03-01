27f62f502e2c87e260d019660d9928066223b91a
Arh <Arh@DESKTOP-8DSVULN>
2024-03-01 16:21:10 +0330

    add protect login middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,30 @@ const { validationResult } = require("express-validator/check"),
     User = require("../models/user");
 
 module.exports = {
+    protect: (req, res, next) => {
+        const authHeader = req.get("Authorization");
+        if (!authHeader) {
+            const error = new Error("Not authenticated.");
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = authHeader.split(" ")[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, "somesupersecretsecret");
+        } catch (err) {
+            err.statusCode = 500;
+            throw err;
+        }
+        if (!decodedToken) {
+            const error = new Error("Not authenticated.");
+            error.statusCode = 401;
+            throw error;
+        }
+        req.userId = decodedToken.userId;
+        next();
+    },
+
     signUp: (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
